c29a78c44a85a1be9d9bde5e5235024ac5ccbbc3
Anna Brands <abrands@endor.ai>
2022-08-10 13:52:49 +0200

    Finally, you can supply an output path

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Hacked-together way to get a Snyk callgraph from a maven project
 
 To run, do the following:
 - (Only for the first run) **Build the maven project, since maven relies on the `target` directory to get the classpath.**
-- Execute `npm run gen absolute/path/to/project`
+- Execute `npm run gen '/absolute/path/to/project' 'path/to/file.json'`
--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,22 +1,37 @@
+import * as fs from 'fs';
 import { EOL } from 'os';
 import { Arguments, CommandBuilder } from 'yargs';
 import { getCallGraph } from '../index';
 
 type Options = {
     targetPath: string,
-}
+    outFile: string | undefined,
+};
 
-export const command: string = 'generate <targetPath>'
+export const command: string = 'generate <targetPath> <outFile>';
 export const desc: string = "Generate snyk call graph";
 
 export const builder: CommandBuilder<Options, Options> = (yargs) =>
     yargs
         .positional('targetPath', { type: 'string', demandOption: true })
+        .positional('outFile', { type: 'string', description: "Output file", demandOption: false })
+
 
 export async function handler(argv: Arguments<Options>): Promise<void> {
-    const { targetPath } = argv
+    const { targetPath, outFile } = argv
+    process.stdout.write("Waiting for call graph generation..." + EOL)
     const result = await getCallGraph(targetPath)
-    process.stdout.write(result)
-    // To make sure it gets printed nicely.
-    process.stdout.write(EOL)
+    process.stdout.write(`Writing to ${outFile}...` + EOL)
+
+    if (outFile !== undefined) {
+        try {
+            fs.writeFileSync(outFile, result)
+        } catch (error) {
+            throw error
+        }
+    } else {
+        process.stdout.write("Writing result to stdout:" + EOL)
+        process.stdout.write(result)
+    }
+
 }
