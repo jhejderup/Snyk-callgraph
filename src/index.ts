c95808f4a507c0769b1a1793830309082e615740
Anna Brands <abrands@endor.ai>
2022-08-10 12:00:48 +0200

    Attempt at getting maven to run

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,53 @@
 #! /usr/bin/env node
-import { getCallGraphGenCommandArgs } from '@snyk/java-call-graph-builder/dist/java-wrapper';
-import * as promisifedFs from '@snyk/java-call-graph-builder/dist/promisified-fs-glob';
-import { execute } from '@snyk/java-call-graph-builder/dist/sub-process';
-import * as path from 'path';
-import * as tempDir from 'temp-dir';
+import { Edge } from '@snyk/graphlib'
+import { getCallGraphMvn } from '@snyk/java-call-graph-builder'
+import { sep as seperator  } from 'path'
 
-export async function getCallGraph(
-  classPath: string,
-  targetPath: string,
-  targets: string[],
-): Promise<string> {
+// export async function getCallGraph(
+//   classPath: string,
+//   targetPath: string,
+//   targets: string[],
+// ): Promise<string> {
 
-  const jarPath = "callgraph-generator.jar"
-  const tmpDir = await promisifedFs.mkdtemp(
-    path.join(tempDir, 'call-graph-generator'),
-  );
-  const classPathFile = path.join(tmpDir, 'callgraph-classpath');
-  await promisifedFs.writeFile(classPathFile, classPath);
+//   const jarPath = "callgraph-generator.jar"
+//   const tmpDir = await promisifedFs.mkdtemp(
+//     path.join(tempDir, 'call-graph-generator'),
+//   );
+//   const classPathFile = path.join(tmpDir, 'callgraph-classpath');
+//   await promisifedFs.writeFile(classPathFile, classPath);
 
 
-  const callgraphGenCommandArgs = getCallGraphGenCommandArgs(
-    classPathFile,
-    jarPath,
-    targets,
-  )
+//   const callgraphGenCommandArgs = getCallGraphGenCommandArgs(
+//     classPathFile,
+//     jarPath,
+//     targets,
+//   )
 
-  try {
-    const javaOutput = await execute('java', callgraphGenCommandArgs, { cwd: targetPath })
-    return javaOutput
-  } finally {
-    cleanupTempDir(classPathFile, tmpDir)
+//   try {
+//     const javaOutput = await execute('java', callgraphGenCommandArgs, { cwd: targetPath })
+//     return javaOutput
+//   } finally {
+//     cleanupTempDir(classPathFile, tmpDir)
+//   }
+// }
+
+// async function cleanupTempDir(classPathFile: string, tmpDir: string): Promise<void> {
+//   try {
+//     await promisifedFs.unlink(classPathFile)
+//     await promisifedFs.rmdir(tmpDir)
+//   } catch {
+//     // we couldn't delete temporary data in temporary folder, no big deal
+//   }
+// }
+
+export async function getCallGraph(path: string): Promise<string> {
+  const graph = await getCallGraphMvn(path)
+  for (const n in graph.nodes) {
+    const temp = graph.nodeEdges(n)
+    const edges = temp instanceof Object ? temp as Edge[] : []
+    for (const edge in edges) {
+      console.log(edge)
+    }
   }
+  return "TODO"
 }
-
-async function cleanupTempDir(classPathFile: string, tmpDir: string): Promise<void> {
-  try {
-    await promisifedFs.unlink(classPathFile)
-    await promisifedFs.rmdir(tmpDir)
-  } catch {
-    // we couldn't delete temporary data in temporary folder, no big deal
-  }
-}
-
